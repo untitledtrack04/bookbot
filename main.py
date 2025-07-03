c8caf6afe9029b72657335645f1360d6863f7dea
forxst <f0rrxst@proton.me>
2025-07-03 14:27:33 -0600

    add code from lessons up until Ch3:L1

---
+++ main.py
@@ -0,0 +1,7 @@
+from stats import sort_dictionary_by_occurrence
+
+def main ():
+    print(sort_dictionary_by_occurrence("./books/frankenstein.txt"))
+
+if __name__=="__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,30 @@
+from test.test_configparser import SortedDict
+from pstats import SortKey
+from optparse import Values
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return(file_contents)
+
+def get_num_words(file_path):
+    book_txt = get_book_text(file_path)
+    book_txt_array = book_txt.split()
+    num_words = len(book_txt_array)
+    return(f'{num_words} words found in the document')
+
+def get_num_chars(file_path):
+    book_txt = get_book_text(file_path)
+    book_txt_lowered = book_txt.lower()
+    book_txt_array = list(book_txt_lowered)
+    char_counts = {}
+    for i in book_txt_array:
+        if i in char_counts:
+            char_counts[i] += 1
+        else:
+            char_counts[i] = 1
+    return(char_counts)
+
+def sort_dictionary_by_occurrence(file_path):
+    dictionary = get_num_chars(file_path)
+    list_of_dicts = {k: v for k, v in sorted(dictionary.items(), key=lambda item:item[1], reverse=True)}
+    return(list_of_dicts)
