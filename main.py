3ef4953e8d759c6bff94054e5bf98c43e90a8dec
forxst <f0rrxst@proton.me>
2025-07-09 00:52:07 -0600

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import print_report
+import sys
 
 def main ():
-    file_path = "books/frankenstein.txt"
-    print_report(file_path)
+    if len(sys.argv) <2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        print_report(file_path)
 
 if __name__=="__main__":
     main()
